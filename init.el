bd05df0ed1a9936fb630340429192f596f0990a3
Test User <will.toher.0@gmail.com>
2023-08-25 16:49:06 -0700

    tweaks

--- init.el
+++ init.el
@@ -183,72 +183,61 @@
   :bind (:map minibuffer-local-map ("M-A" . marginalia-cycle))
   :init (marginalia-mode))
 
-;; (use-package ivy
-;;   :config (ivy-mode)
-;;   :bind (:map ivy-mode-map
-;;        ("C-j" . 'ivy-next-line)
-;;        ("C-k" . 'ivy-previous-line)))
-
-;; (use-package counsel
-;;   :after ivy
-;;   :config (counsel-mode))
-
-;; (use-package swiper
-;;   :after ivy)
-
 (use-package consult
-  ;; Replace bindings. Lazily loaded due by `use-package'.
   :bind (;; C-c bindings in `mode-specific-map'
-         ("C-c M-x" . consult-mode-command)
-         ("C-c h" . consult-history)
-         ("C-c k" . consult-kmacro)
-         ("C-c m" . consult-man)
-         ("C-c i" . consult-info)
+         ("C-c M-x"           . consult-mode-command)
+         ("C-c h"             . consult-history)
+         ("C-c k"             . consult-kmacro)
+         ("C-c m"             . consult-man)
+         ("C-c i"             . consult-info)
          ([remap Info-search] . consult-info)
          ;; C-x bindings in `ctl-x-map'
-         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
-         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
-         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
-         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
-         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
-         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ("C-x M-:"           . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b"             . consult-buffer)              ;; orig. switch-to-buffer
+         ("C-x 4 b"           . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b"           . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x r b"           . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b"           . consult-project-buffer)      ;; orig. project-switch-to-buffer
          ;; Custom M-# bindings for fast register access
-         ("M-#" . consult-register-load)
-         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
-         ("C-M-#" . consult-register)
+         ("M-#"               . consult-register-load)
+         ("M-'"               . consult-register-store)      ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#"             . consult-register)
          ;; Other custom bindings
-         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ("M-y"               . consult-yank-pop)            ;; orig. yank-pop
          ;; M-g bindings in `goto-map'
-         ("M-g e" . consult-compile-error)
-         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
-         ("M-g g" . consult-goto-line)             ;; orig. goto-line
-         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
-         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
-         ("M-g m" . consult-mark)
-         ("M-g k" . consult-global-mark)
-         ("M-g i" . consult-imenu)
-         ("M-g I" . consult-imenu-multi)
+         ("M-g e"             . consult-compile-error)
+         ("M-g f"             . consult-flycheck)
+         ("M-g g"             . consult-goto-line)           ;; orig. goto-line
+         ("M-g M-g"           . consult-goto-line)           ;; orig. goto-line
+         ("M-g o"             . consult-outline)
+         ("M-g m"             . consult-mark)
+         ("M-g k"             . consult-global-mark)
+         ("M-g i"             . consult-imenu)
+         ("M-g I"             . consult-imenu-multi)
          ;; M-s bindings in `search-map'
-         ("M-s d" . consult-find)
-         ("M-s D" . consult-locate)
-         ("M-s g" . consult-grep)
-         ("M-s G" . consult-git-grep)
-         ("M-s r" . consult-ripgrep)
-         ("M-s l" . consult-line)
-         ("M-s L" . consult-line-multi)
-         ("M-s k" . consult-keep-lines)
-         ("M-s u" . consult-focus-lines)
+         ("M-s d"             . consult-find)
+         ("M-s D"             . consult-locate)
+         ("M-s g"             . consult-grep)
+         ("M-s G"             . consult-git-grep)
+         ("M-s r"             . consult-ripgrep)
+         ("M-s l"             . consult-line)
+         ("M-s L"             . consult-line-multi)
+         ("M-s k"             . consult-keep-lines)
+         ("M-s u"             . consult-focus-lines)
          ;; Isearch integration
-         ("M-s e" . consult-isearch-history)
+         ("M-s e"             . consult-isearch-history)
          :map isearch-mode-map
-         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
-         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
-         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
-         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ("M-e"               . consult-isearch-history)     ;; orig. isearch-edit-string
+         ("M-s e"             . consult-isearch-history)     ;; orig. isearch-edit-string
+         ("M-s l"             . consult-line)                ;; ne
+         ("M-s L"             . consult-line-multi)          ;; ne
          ;; Minibuffer history
          :map minibuffer-local-map
-         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
-         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+         ("M-s"               . consult-history)
+         ("M-r"               . consult-history)
+         :map org-mode-map
+         ("M-g o"             . consult-org-heading))
+
 
   ;; Enable automatic preview at point in the *Completions* buffer. This is
   ;; relevant when you use the default completion UI.
@@ -342,10 +331,6 @@
 
 (use-package embark-consult
   :hook (embark-collect-mode . consult-preview-at-point-mode))
-
-;; (use-package org-superstar
-;;   :after (org)
-;;   :config (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1))))
 
 (use-package toc-org
   :after (org)
